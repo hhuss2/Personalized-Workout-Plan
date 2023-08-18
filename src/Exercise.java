689705b2cf6274a43b40d8efa3f7cfceb5a0d337
hhuss2 <142123978+hhuss2@users.noreply.github.com>
2023-08-18 00:42:51 -0400

    Update Exercise.java

--- src/Exercise.java
+++ src/Exercise.java
@@ -1,1 +1,34 @@
+public class Exercise {
+    private String name;
+    private String muscleGroup;
+    private String equipment;
+    private int sets; // Add this field
 
+    public Exercise(String name, String muscleGroup, String equipment, int sets) {
+        this.name = name;
+        this.muscleGroup = muscleGroup;
+        this.equipment = equipment;
+        this.sets = sets; // Initialize the sets
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMuscleGroup() {
+        return muscleGroup;
+    }
+
+    public String getEquipment() {
+        return equipment;
+    }
+
+    public int getSets() {
+        return sets;
+    }
+
+    @Override
+    public String toString() {
+        return "Exercise: " + name + "\nMuscle Group: " + muscleGroup + "\nEquipment: " + equipment;
+    }
+}
